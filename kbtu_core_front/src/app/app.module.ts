fd49b588d880fa7e10eb4488493c3b5f88a9545c
andreywalker <andreyetoja@gmail.com>
2023-04-01 11:47:15 +0600

    Andrey: added material, top app bar

--- kbtu_core_front/src/app/app.module.ts
+++ kbtu_core_front/src/app/app.module.ts
@@ -3,25 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
 import { CommonModule } from '@angular/common';  
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent
+    LoginPageComponent,
     ContentListComponent,
     TopBarComponent,
   ],
   imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatSlideToggleModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
