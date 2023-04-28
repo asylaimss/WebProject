922eb0189d920bfa834c074955749ed328b11568
ArmaniKE <armani.kabidolda.18.12.03@gmail.com>
2023-04-28 15:26:58 +0600

    Arman monstr

--- kbtu_core_front/src/app/login-page/login-page.component.ts
+++ kbtu_core_front/src/app/login-page/login-page.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit {
+  signupUsers: any[] = [];
+  
+  loginObj: any = {
+    username: '',
+    password: '',
+  };
+  
+  constructor() { }
 
+  ngOnInit(): void {
+
+  }
+
+  onLogin() {
+    this.signupUsers.push(this.loginObj);
+    localStorage.setItem('signupUsers', JSON.stringify(this.signupUsers));
+    this.loginObj = {
+      username: '',
+      password: '',
+    };
+  }
 }
--- kbtu_core_front/src/app/sign-up/sign-up.component.ts
+++ kbtu_core_front/src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent {
+  signupUsers: any[] = [];
+  newUser: string;
+  newPassword: string;
 
+  signupObj: any = {
+    username: '',
+    password: '',
+  };
+
+  constructor() { 
+    this.newUser = '';
+    this.newPassword = '';
+
+  }
+
+  ngOnInit(): void {
+    const savedUsers = localStorage.getItem('signupUsers');
+    if (savedUsers) {
+      this.signupUsers = JSON.parse(savedUsers);
+    }
+  }
+
+  onSignup() {
+    this.signupObj = {username: this.newUser, password: this.newPassword};
+    this.signupUsers.push(this.signupObj);
+    localStorage.setItem('signupUsers', JSON.stringify(this.signupUsers));
+    
+  }
+  
 }
